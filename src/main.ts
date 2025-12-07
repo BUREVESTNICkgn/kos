de2b9d1508a7f9e429966af87978229125658ebb
BUREVESTNICkgn <tigrantarifyan@yandex.com>
2025-12-07 21:27:48 +0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app';
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideAnimations()]
+}).catch(err => console.error(err));
